80e4c830567aa48358f769e322b5569a14368bfd
matheusalcatrao@hotmail.com <matheusalcatrao@hotmail.com>
2023-09-28 00:21:40 -0300

    Initial Commit

---
+++ alura-viagens-02/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  alura-viagens-02
+//
+//  Created by Matheus Cepil Alcatrao on 28/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alura-viagens-02/alura_viagens_02App.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagens_02App.swift
+//  alura-viagens-02
+//
+//  Created by Matheus Cepil Alcatrao on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagens_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
